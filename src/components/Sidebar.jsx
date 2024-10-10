b4a9f523b0872d8cc6baee7859d01fcd601dd634
alimirhashimli1 <alimirhashimli@gmail.com>
2024-10-10 23:40:57 +0200

    changed the padding of the logo

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -15,8 +15,8 @@ const Sidebar = () => {
   return (
     <div className="w-64 h-auto bg-[#F0FBFF] text-black font-bold flex flex-col shadow-lg">
       {/* Logo */}
-      <Link to="/dashboard" className="p-4">
-        <img src={alitheaBio} alt="alithea logo" className="w-full h-auto" />
+      <Link to="/dashboard" className="pt-7 px-2">
+        <img src={alitheaBio} alt="alithea logo" className="w-full h-auto " />
       </Link>
 
       {/* Navigation */}
